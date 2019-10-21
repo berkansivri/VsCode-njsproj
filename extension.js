c48a8809f445587658326d16ef23887d4e691a24
berkansivri <berkansivri@gmail.com>
2019-10-21 17:17:20 +0300

    support for multiple drag and drop files, more support on nested deletion

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-const vscode = require('vscode')
+const { workspace, window } = require('vscode')
 const path = require('path')
 const fs = require('fs')
 const xml2js = require('xml2js')
@@ -6,25 +6,23 @@ const xml2js = require('xml2js')
 
 const builder = new xml2js.Builder()
 let isWriteFinished = true
+let fileWatcher = null
+let njsprojFile = null
 
 function activate(context) {
-  console.log("ACTIVATED");
-
-  const fileWatcher = vscode.workspace.createFileSystemWatcher('**/*', false, true, false)
+  console.log("ACTIVATED")
+  fileWatcher = workspace.createFileSystemWatcher('**/*', false, true, false)
   fileWatcher.onDidCreate(async (e) => {
     console.log("create")
     try {
-
       await waitForProcess()
-      isWriteFinished = false
 
-      const njsFile = await getNjsprojFile();
-      const xmlObj = await getNjsprojContent(njsFile)
+      njsprojFile = await getNjsprojFile()
+      const xmlObj = await getNjsprojContent()
       const [content1, folder] = xmlObj.Project.ItemGroup
-
       const isFile = fs.lstatSync(e.fsPath).isFile()
       if (isFile) {
-        let filePath = path.normalize(vscode.workspace.asRelativePath(e.fsPath))
+        let filePath = path.normalize(workspace.asRelativePath(e.fsPath))
         const isFileExist = content1.Content.map(f => f.$.Include).includes(filePath)
         if (!isFileExist) {
           content1.Content.push({
@@ -34,7 +32,7 @@ function activate(context) {
           })
         }
       } else {
-        let folderPath = path.normalize(vscode.workspace.asRelativePath(e.fsPath)) + "\\"
+        let folderPath = path.normalize(workspace.asRelativePath(e.fsPath)) + path.sep
         const isFolderExist = folder.Folder.map(f => f.$.Include).includes(folderPath)
         if (!isFolderExist) {
           folder.Folder.push({
@@ -44,13 +42,13 @@ function activate(context) {
           })
         }
       }
-
-      const finalXml = builder.buildObject(xmlObj)
-      fs.writeFileSync(njsFile.fsPath, finalXml, 'utf8')
-      vscode.window.showInformationMessage('.njsproj updated')
+      
+      writeNjsprojFile(xmlObj)
+      window.showInformationMessage('.njsproj updated')
     } catch (ex) {
-      console.log(ex);
-      if (ex.message) vscode.window.showWarningMessage(err.message)
+      console.log(ex)
+      if (ex.message) window.showWarningMessage(err.message)
+      else window.showWarningMessage("VsCode .njsproj failure")
       isWriteFinished = true
     }
     isWriteFinished = true
@@ -60,13 +58,12 @@ function activate(context) {
     console.log("delete")
     try {
       await waitForProcess()
-      isWriteFinished = false
 
-      const njsFile = await getNjsprojFile();
-      const xmlObj = await getNjsprojContent(njsFile)
+      njsprojFile = await getNjsprojFile()
+      const xmlObj = await getNjsprojContent()
       const [content1, folder, content2] = xmlObj.Project.ItemGroup
 
-      let deletedPath = path.normalize(vscode.workspace.asRelativePath(e.fsPath))
+      let deletedPath = path.normalize(workspace.asRelativePath(e.fsPath))
 
       const deletedFolders = folder.Folder.filter(f => f.$.Include.slice(0, -1).includes(deletedPath))
 
@@ -88,32 +85,38 @@ function activate(context) {
         }
       }
 
-      const finalXml = builder.buildObject(xmlObj)
-      fs.writeFileSync(njsFile.fsPath, finalXml, 'utf8')
-      vscode.window.showInformationMessage('.njsproj updated')
+      writeNjsprojFile(xmlObj)
+      window.showInformationMessage('.njsproj updated')
     } catch (ex) {
-      console.log(ex);
-      if (ex.message) vscode.window.showWarningMessage(ex.message)
+      console.log(ex)
+      if (ex.message) window.showWarningMessage(ex.message)
+      else window.showWarningMessage("VsCode .njsproj failure")
       isWriteFinished = true
     }
     isWriteFinished = true
   })
 }
-exports.activate = activate
 
-function deactivate() {}
+function deactivate() {
+  fileWatcher.dispose()
+}
+
+function writeNjsprojFile(xmlObj) {
+  const finalXml = builder.buildObject(xmlObj)
+  fs.writeFileSync(njsprojFile.fsPath, finalXml, 'utf8')
+}
 
 async function getNjsprojFile() {
-  const njsprojFile = await vscode.workspace.findFiles('**/*.njsproj')
+  const njsprojFile = await workspace.findFiles('**/*.njsproj')
   if (njsprojFile.length === 0) {
     deactivate()
     return []
   } else return njsprojFile[0]
 }
 
-async function getNjsprojContent(njsProjFile) {
-  const content = await vscode.workspace.openTextDocument(njsProjFile.path)
-  const xmlObj = await xml2js.parseStringPromise(content.getText())
+async function getNjsprojContent() {
+  const njsDocument = await workspace.openTextDocument(njsprojFile.path)
+  const xmlObj = await xml2js.parseStringPromise(njsDocument.getText())
   return xmlObj
 }
 
@@ -124,8 +127,10 @@ async function waitForProcess() {
   while (!isWriteFinished) {
     await timer(50)
   }
-  return
+  isWriteFinished = false
 }
+
+exports.activate = activate
 
 module.exports = {
   activate,

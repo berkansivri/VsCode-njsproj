339a549266c4a84ee84aaee5283412a734c25f87
berkansivri <berkansivri@gmail.com>
2019-10-22 12:09:46 +0300

    supports for workspace with multiple projects

--- extension.js
+++ extension.js
@@ -11,40 +11,44 @@ let njsprojFile = null
 
 function activate(context) {
   console.log("ACTIVATED")
+
+  workspace.findFiles('**/*.njsproj', '**/node_modules/**', 1).then(arr => arr.length === 0 && deactivate())
+
   fileWatcher = workspace.createFileSystemWatcher('**/*', false, true, false)
   fileWatcher.onDidCreate(async (e) => {
-    console.log("create")
     try {
       await waitForProcess()
 
-      njsprojFile = await getNjsprojFile()
-      const xmlObj = await getNjsprojContent()
-      const [content1, folder] = xmlObj.Project.ItemGroup
-      const isFile = fs.lstatSync(e.fsPath).isFile()
-      if (isFile) {
-        let filePath = path.normalize(workspace.asRelativePath(e.fsPath))
-        const isFileExist = content1.Content.map(f => f.$.Include).includes(filePath)
-        if (!isFileExist) {
-          content1.Content.push({
-            $: {
-              Include: filePath
-            }
-          })
+      njsprojFile = await getNjsprojFile(e)
+      if (njsprojFile) {
+        const xmlObj = await getNjsprojContent()
+        const [content1, folder] = xmlObj.Project.ItemGroup
+        const isFile = fs.lstatSync(e.fsPath).isFile()
+        if (isFile) {
+          let filePath = path.normalize(workspace.asRelativePath(e.fsPath))
+          const isFileExist = content1.Content.map(f => f.$.Include).includes(filePath)
+          if (!isFileExist) {
+            content1.Content.push({
+              $: {
+                Include: filePath
+              }
+            })
+          }
+        } else {
+          let folderPath = path.normalize(workspace.asRelativePath(e.fsPath)) + path.sep
+          const isFolderExist = folder.Folder.map(f => f.$.Include).includes(folderPath)
+          if (!isFolderExist) {
+            folder.Folder.push({
+              $: {
+                Include: folderPath
+              }
+            })
+          }
         }
-      } else {
-        let folderPath = path.normalize(workspace.asRelativePath(e.fsPath)) + path.sep
-        const isFolderExist = folder.Folder.map(f => f.$.Include).includes(folderPath)
-        if (!isFolderExist) {
-          folder.Folder.push({
-            $: {
-              Include: folderPath
-            }
-          })
-        }
+
+        writeNjsprojFile(xmlObj)
+        window.showInformationMessage('.njsproj updated')
       }
-      
-      writeNjsprojFile(xmlObj)
-      window.showInformationMessage('.njsproj updated')
     } catch (ex) {
       console.log(ex)
       if (ex.message) window.showWarningMessage(err.message)
@@ -55,38 +59,40 @@ function activate(context) {
   })
 
   fileWatcher.onDidDelete(async (e) => {
-    console.log("delete")
     try {
       await waitForProcess()
 
-      njsprojFile = await getNjsprojFile()
-      const xmlObj = await getNjsprojContent()
-      const [content1, folder, content2] = xmlObj.Project.ItemGroup
+      njsprojFile = await getNjsprojFile(e)
+      if (njsprojFile) {
+        const xmlObj = await getNjsprojContent()
+        const [content1, folder, content2] = xmlObj.Project.ItemGroup
 
-      let deletedPath = path.normalize(workspace.asRelativePath(e.fsPath))
+        let deletedPath = path.normalize(workspace.asRelativePath(e.fsPath))
 
-      const deletedFolders = folder.Folder.filter(f => f.$.Include.slice(0, -1).includes(deletedPath))
+        const deletedFolders = folder.Folder.filter(f => f.$.Include.slice(0, -1).includes(deletedPath))
 
-      if (deletedFolders.length > 0) {
-        // Remove nested folders
-        folder.Folder = folder.Folder.filter(f => !deletedFolders.includes(f))
-        // Remove Files Under Deleted Folder
-        content1.Content = content1.Content.filter(f => !path.dirname(f.$.Include).includes(deletedPath))
-        content2.Content = content2.Content.filter(f => !path.dirname(f.$.Include).includes(deletedPath))
-      } else {
-        let fileIndex = content1.Content.map(f => f.$.Include).indexOf(deletedPath)
-        if (fileIndex > -1) {
-          content1.Content.splice(fileIndex, 1)
+        if (deletedFolders.length > 0) {
+          // Remove nested folders
+          folder.Folder = folder.Folder.filter(f => !deletedFolders.includes(f))
+          // Remove Files Under Deleted Folder
+          content1.Content = content1.Content.filter(f => !isRelative(path.dirname(deletedPath), path.dirname(f.$.Include)))
+          content2.Content = content2.Content.filter(f => !isRelative(path.dirname(deletedPath), path.dirname(f.$.Include)))
+          // content2.Content = content2.Content.filter(f => !path.dirname(f.$.Include).includes(deletedPath))
         } else {
-          fileIndex = content2.Content.map(f => f.$.Include).indexOf(deletedPath)
+          let fileIndex = content1.Content.map(f => f.$.Include).indexOf(deletedPath)
           if (fileIndex > -1) {
-            content2.Content.splice(fileIndex, 1)
+            content1.Content.splice(fileIndex, 1)
+          } else {
+            fileIndex = content2.Content.map(f => f.$.Include).indexOf(deletedPath)
+            if (fileIndex > -1) {
+              content2.Content.splice(fileIndex, 1)
+            }
           }
         }
-      }
 
-      writeNjsprojFile(xmlObj)
-      window.showInformationMessage('.njsproj updated')
+        writeNjsprojFile(xmlObj)
+        window.showInformationMessage('.njsproj updated')
+      }
     } catch (ex) {
       console.log(ex)
       if (ex.message) window.showWarningMessage(ex.message)
@@ -98,6 +104,7 @@ function activate(context) {
 }
 
 function deactivate() {
+  console.log("DEACTIVATE");
   fileWatcher.dispose()
 }
 
@@ -106,12 +113,18 @@ function writeNjsprojFile(xmlObj) {
   fs.writeFileSync(njsprojFile.fsPath, finalXml, 'utf8')
 }
 
-async function getNjsprojFile() {
-  const njsprojFile = await workspace.findFiles('**/*.njsproj')
-  if (njsprojFile.length === 0) {
+async function getNjsprojFile(e) {
+  const njsprojFiles = await workspace.findFiles('**/*.njsproj', '**/node_modules/**', 5)
+  if (njsprojFiles.length === 0) {
     deactivate()
-    return []
-  } else return njsprojFile[0]
+  } else {
+    const njsFile = njsprojFiles.find(njs => {
+      if (isRelative(path.dirname(njs.fsPath), path.dirname(e.fsPath))) {
+        return njs
+      }
+    })
+    return njsFile
+  }
 }
 
 async function getNjsprojContent() {
@@ -128,6 +141,11 @@ async function waitForProcess() {
     await timer(50)
   }
   isWriteFinished = false
+}
+
+function isRelative(parent, dir) {
+  const relative = path.relative(parent, dir)
+  return relative && !relative.startsWith('..') && !path.isAbsolute(relative)
 }
 
 exports.activate = activate
